8521da07710f1c95ab73b40704d7fabb0ce26784
“together08” <together08@yeah.net>
2021-02-14 20:02:56 +0800

    whyred: exTHmUI Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_whyred.mk
+    $(LOCAL_DIR)/exthm_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_whyred-user \
-    lineage_whyred-userdebug \
-    lineage_whyred-eng
+    exthm_whyred-user \
+    exthm_whyred-userdebug \
+    exthm_whyred-eng
